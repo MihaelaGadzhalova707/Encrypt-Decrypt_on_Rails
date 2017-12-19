622c5b23c412f92085de0afba9e79164bb6f8924
MihaelaGadzhalova707 <mihaelagadzhalova@gmail.com>
2017-12-20 01:26:18 +0200

    this code is absolutely useless

---
+++ vulcano/app/controllers/encrypt_controller.rb
@@ -0,0 +1,41 @@
+require "RSA"
+
+class DecryptMessagesController < ApplicationController
+	before_action :set_message, only: [:show]
+
+	def create
+
+		msg = DecryptMessage.new()
+
+		mesg.content = params[:msg]
+
+		key = Key.find(params[:id])
+
+		rsa = RSA.new(key.n, key.e, key.d)
+
+		message.content = rsa.decrypt(message.content)
+
+		respond_to do |format|
+			if msg.save
+				format.json {
+					render :json => {'message' => msg.content}
+				}
+			end
+		end
+	end
+
+	def show
+		msg = EncryptMessage.find(params[:id])
+
+		#write some code here
+	end
+
+private
+	def set_message
+		@msg = EncryptMessage.find(params[:id])
+	end
+
+	def message_params
+		#write some code here
+	end
+end
